e1f3d21401f16710d7e118d5c9826aa4349ed889
Lenisa Marisco <162896124+lenisamarisco@users.noreply.github.com>
2024-11-07 19:20:57 -0500

    Second parte

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """SQLAlchemy models for blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+import datetime  # Import datetime module for timestamps
 
 db = SQLAlchemy()
 
@@ -20,8 +21,28 @@ class User(db.Model):
     @property
     def full_name(self):
         """Return full name of user."""
-
         return f"{self.first_name} {self.last_name}"
+
+
+class Post(db.Model):
+    """Blog post."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now  # Use correct datetime default
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
+        return self.created_at.strftime("%a %b %-d %Y, %-I:%M %p")
 
 
 def connect_db(app):
@@ -29,6 +50,5 @@ def connect_db(app):
 
     You should call this in your Flask app.
     """
-
     db.app = app
     db.init_app(app)
